572970bb0d98535e0f784997bc30e7e16b552b1a
Olga Aukhadieva <sarole4ka@gmail.com>
2024-08-04 08:36:44 +0500

    Add get_db func

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+
 from starlette.templating import Jinja2Templates
 
 import models
-from database import engine
+from database import SessionLocal, engine
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 templates = Jinja2Templates(directory='templates')
+
+
+def get_db():
+    """
+    Создает и возвращает экземпляр сессии базы данных.
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
